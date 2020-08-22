6e2a42a22c3019110d0703e98728df48feeb638d
Wendy Kelly <wendykelly@Wendys-iMac.local>
2020-08-22 16:11:05 -0700

    typo

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -183,7 +183,7 @@ export async function getStaticPaths() {
        .trim()
    )
    // create paths with `slug` param
-  const paths = blogSlugs.mapslug => `/posts/${slug}`)
+  const paths = blogSlugs.map(slug => `/posts/${slug}`)
   return {
     paths,
     fallback: false,
