5719bde9b2e6588176de3f79f8879564eaae78a9
Wendy Kelly <wendykelly@Wendys-iMac.local>
2020-08-22 15:44:15 -0700

    syntax on config

--- next.config.js
+++ next.config.js
@@ -43,15 +43,11 @@ module.exports = withPlugins(
     withCSS,
   ],
 
+)
   webpack: function(config) {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
     })
     return config
-  },
-
-
-
-
-);
+  };
