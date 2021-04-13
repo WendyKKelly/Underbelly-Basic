3b8696bfa86dd47cf74e5a1dea3f000ba62593a1
Wendy Kelly <wendykelly@Wendys-iMac.local>
2021-04-13 13:02:10 -0700

    changed colors back to proper ones

--- styles/theme/agencyDigital/colors.js
+++ styles/theme/agencyDigital/colors.js
@@ -2,8 +2,8 @@ const colors = {
   transparent: 'transparent', // 0
   black: '#000000', // 1
   white: '#ffffff', // 2
-  text: '#343D48', // 3
-  textPrimary: '#02073E', // 4
+  text: '#2c2e35', // 3
+  textPrimary: '#2c2e35', // 4
   labelColor: '#767676', // 5
   inactiveField: '#f2f2f2', // 6
   border: '#EDEEF0',
@@ -13,9 +13,9 @@ const colors = {
   primaryHover: '#FF825C', // 9
   secondary: '#ff5b60', // 10
   secondaryHover: '#FF282F', // 11
-  yellow: '#fdb32a', // 12
+  yellow: '#ee9b27', // 12
   yellowHover: '#F29E02', // 13
-  linkColor: '#3183FF',
+  linkColor: '#2b7b2f',
   slogan: '#00A99D',
   primaryBoxShadow: ' 0px 9px 19.74px 1.26px rgba(82, 104, 219, 0.57) ',
 };
