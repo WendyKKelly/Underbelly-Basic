5844bb0d53638d3a35d4dee7c95bbdcb440867d2
Wendy Kelly <wendykelly@Wendys-iMac.local>
2021-04-19 19:56:12 -0700

    minor changes

--- components/Explain/explain.style.js
+++ components/Explain/explain.style.js
@@ -87,7 +87,6 @@ export const BannerContent = styled.div`
   .head {
       display: flex;
       
-
   }
   h1 {
     font-family: Helvetica Neue;
@@ -228,6 +227,7 @@ export const BannerContent = styled.div`
         @media only screen and (max-width: 480px) {
             font-weight: 900; font-size: 4rem;
         }
+    
         .p {
             font-family: Playfair Display; 
             font-weight: 900;
@@ -249,120 +249,6 @@ export const BannerContent = styled.div`
                 
               font-size: 4rem;
             }
-  }
-`;
-
-export const Subscribe = styled.div`
-  display: flex;
-  margin-top: 40px;
-  @media only screen and (max-width: 1440px) {
-    margin-top: 30px;
-  }
-  @media only screen and (max-width: 768px) {
-    max-width: 80%;
-    margin: 0 auto;
-  }
-  @media only screen and (max-width: 480px) {
-    max-width: 100%;
-    margin: 0 auto;
-    flex-direction: column;
-  }
-
-  @media only screen and (max-width: 480px) {
-    align-items: center;
-  }
-  .reusecore__input {
-    width: 100%;
-  }
-  .field-wrapper {
-    margin-right: 15px;
-    @media only screen and (max-width: 480px) {
-      margin-right: 0px;
-    }
-    input {
-      font-family: DM Sans;
-      font-size: 16px;
-      min-height: 60px;
-      padding: 0 24px;
-      border-radius: 8px;
-      ::placeholder {
-        color: ${rgba('#02073E', 0.4)};
-        opacity: 1; /* Firefox */
-      }
-      &:focus {
-        border-color: #ff825c;
-      }
-
-      @media only screen and (max-width: 1280px) {
-        min-height: 50px;
-      }
-    }
-  }
-  button {
-    background-color: #ff825c;
-    min-width: 150px;
-    border-radius: 8px;
-
-    @media only screen and (max-width: 480px) {
-      min-width: 100%;
-      margin-top: 15px;
-    }
-  }
-`;
-
-export const SponsoredBy = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 40px;
-  @media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.5) {
-    display: block;
-  }
-  @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape) {
-    display: block;
-    margin-top: 25px;
-  }
-  @media only screen and (max-width: 1440px) {
-    margin-top: 30px;
-  }
-  @media only screen and (max-width: 768px) {
-    margin-top: 30px;
-    text-align: center;
-    justify-content: center;
-  }
-
-  @media only screen and (max-width: 480px) {
-    align-items: flex-start;
-    flex-direction: column;
-  }
-
-  .sponsoredBy {
-    color: ${rgba('#566272', 0.6)};
-    font-size: 16px;
-    margin-right: 21px;
-    margin-bottom: 0;
-    @media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.5) {
-      margin: 0 0 20px 0;
-    }
-    @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape) {
-      margin: 0 0 15px 0;
-    }
-    @media only screen and (max-width: 480px) {
-      margin-bottom: 15px;
-    }
-  }
-`;
-
-export const ImageGroup = styled.div`
-  display: flex;
-  align-items: center;
-  img {
-    &:not(:last-child) {
-      margin-right: 23px;
-    }
-
-    @media only screen and (max-width: 480px) {
-      max-width: 27%;
-    }
   }
 `;
 
--- components/Explain/index.js
+++ components/Explain/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Iframe from 'react-iframe';
-import Zoom from 'react-reveal/Zoom';
+
 import Text from '../Text';
 import Link from '../Link';
-import Button from '../Button';
+
 import Heading from '../Heading';
 
 import Container from '../UI/ContainerTwo';
@@ -13,38 +12,37 @@ const Explain = () => {
   return (
     <Section>
       <Container>
-        <Zoom>
-          <ContentWrapper>
-            <BannerContent>
-              <Heading
-                className="head"
-                as="h1"
-                content="Wendy Kelly's Underbelly"
-              />
+        <ContentWrapper>
+          <div className="me"></div>
+          <BannerContent>
+            <Heading
+              className="head"
+              as="h1"
+              content="Wendy Kelly's Underbelly"
+            />
 
-              <Text
-                className="banner-caption"
-                content="How do you go from unloveable to loved? From burned out to deeply connected to the world around you? From wounded and wounding to caring for yourself and others? Curious? Explore my 'delightful', 'always inspiring' and 'delicious' offerings in the daughter project to The Underbelly, 'Wild and Precious' 
+            <Text
+              className="banner-caption"
+              content="How do you go from unloveable to loved? From burned out to deeply connected to the world around you? From wounded and wounding to caring for yourself and others? Curious? Explore my 'delightful', 'always inspiring' and 'delicious' offerings in the daughter project to The Underbelly, 'Wild and Precious' 
                 "
-              />
+            />
 
-              <Link href="/wild-and-precious" passHref>
-                <div className="wp">
-                  <span className="w">Wild </span>
-                  <span
-                    style={{
-                      fontFamily: 'Playfair Display',
-                      fontWeight: '900',
-                      fontSize: '4rem',
-                    }}
-                  >
-                    + Precious
-                  </span>
-                </div>
-              </Link>
-            </BannerContent>
-          </ContentWrapper>
-        </Zoom>
+            <Link href="/wild-and-precious" passHref>
+              <div className="wp">
+                <span className="w">Wild </span>
+                <span
+                  style={{
+                    fontFamily: 'Playfair Display',
+                    fontWeight: '900',
+                    fontSize: '4rem',
+                  }}
+                >
+                  + Precious
+                </span>
+              </div>
+            </Link>
+          </BannerContent>
+        </ContentWrapper>
       </Container>
     </Section>
   );
