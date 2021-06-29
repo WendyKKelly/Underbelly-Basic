98e1c5963d8fa981133a7659f5e88461023b6518
Wendy Kelly <wendykelly@Wendys-iMac.local>
2021-06-29 16:08:20 -0700

    change nav for posts

--- components/postpreviewcard.js
+++ components/postpreviewcard.js
@@ -6,7 +6,7 @@ export default function BlogPreviewCard({ blogpost }) {
     <div className="grid my-12">
       <div className="grid place-self-center w-3/4 grid-cols-1 md:grid-cols-2 mb-8 border-b-2 border-gray-100 border-dotted ">
         <div className="flex justify-start md:justify-center items-center">
-          <Link href="/blog/posts/[slug]" as={`/blog/posts/${blogpost.slug}`}>
+          <Link href="/posts/[slug]" as={`/posts/${blogpost.slug}`}>
             <img className="m-8 rounded-l h-32 " src={blogpost.feature_image} />
           </Link>
         </div>
@@ -15,7 +15,7 @@ export default function BlogPreviewCard({ blogpost }) {
             <p className="text-gray-500 text-sm">
               {blogpost.tags.map((tag) => (
                 <span className="mr-3">
-                  <Link href="/blog/tags/[slug]" as={`/blog/tags/${tag.slug}`}>
+                  <Link href="/tag/[slug]" as={`/tag/${tag.slug}`}>
                     <a>{tag.name}</a>
                   </Link>
                 </span>
@@ -26,7 +26,7 @@ export default function BlogPreviewCard({ blogpost }) {
               (reading time: {blogpost.reading_time} min.)
             </p>
             <hr />
-            <Link href="/blog/posts/[slug]" as={`/blog/posts/${blogpost.slug}`}>
+            <Link href="/posts/[slug]" as={`/posts/${blogpost.slug}`}>
               <a>
                 <h1 className="text-2xl font-bold text-indigo-900">
                   {blogpost.title}
